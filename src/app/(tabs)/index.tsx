e75b77a5bc9c44a01ec3034b69f0b880e84316dc
Vignesh <vigneshpamu2002@gmail.com>
2024-03-17 19:37:16 +0530

    Styling Card

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native'
+import { View, FlatList } from 'react-native'
 import products from '@assets/data/products'
 import ProductListItem from '@components/ProductListItem'
 
@@ -7,8 +7,15 @@ import ProductListItem from '@components/ProductListItem'
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[0]} />
-      <ProductListItem product={products[1]} />
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
+      {/* <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} /> */}
     </View>
   )
 }
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -17,6 +17,7 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
       <Image
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
+        resizeMode="contain"
       />
       <Text style={styles.title}>{product.name} H</Text>
       <Text style={styles?.price}>${product.price}</Text>
@@ -31,6 +32,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
   },
   title: { fontSize: 18, fontWeight: '600', marginVertical: 10 },
   price: { fontWeight: 'bold', color: Colors.light.tint },
